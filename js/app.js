e0902bfea813aaa8ba212e2e2b3451c256b4640a
Yamil Escobar <joseyamilej@hotmail.com>
2022-09-16 00:41:35 +0200

    Mostrando los Gastos en el HTML

--- js/app.js
+++ js/app.js
@@ -67,6 +67,45 @@ class UI {
             divMensaje.remove();
         }, 3000);
     }
+
+    agregarGastoListado(gastos) {
+
+        this.limpiarHTML(); // Elimina el HTML previo
+        // Iterar sobre los gastos
+        gastos.forEach( gasto => {
+            const { cantidad, nombre, id } = gasto;
+
+            // Crear un LI
+            const nuevoGasto = document.createElement('li');
+            nuevoGasto.className = 'list-group-item d-flex justify-content-between align-items-center';
+            nuevoGasto.dataset.id = id;
+
+
+            // Agregar el HTML del gasto
+
+            nuevoGasto.innerHTML = ` ${nombre} <span class="badge badge-primary badge-pill"> ${cantidad} </span>`;
+
+
+            // Boton para borrar el gasto
+
+            const btnBorrar = document.createElement('button');
+            btnBorrar.classList.add('btn', 'btn-danger', 'borrar-gasto');
+            btnBorrar.innerHTML= 'Borrar &times;'
+            nuevoGasto.appendChild(btnBorrar);
+
+            // Agregar al HTML
+
+            gastoListado.appendChild(nuevoGasto);
+
+        });
+    }
+
+    limpiarHTML() {
+        while( gastoListado.firstChild ) {
+            gastoListado.removeChild(gastoListado.firstChild);
+        }
+    }
+
 }
 
 // Instanciar
@@ -120,6 +159,11 @@ function agregarGasto(e) {
     presupuesto.nuevoGasto( gasto );
     
     ui.imprimirAlerta('Gasto agregado Correctamente');
+
+    // Imprimir los gastos
+    const { gastos } = presupuesto;
+    ui.agregarGastoListado(gastos);
+
 
 
     // Reinicia el formulario
